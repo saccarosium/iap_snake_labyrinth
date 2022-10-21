22dbc1bff0698ec5b752977dffdf300002ec6b7c
Friedchicken-42 <paololego20+github@gmail.com>
2022-10-21 18:18:12 +0200

    Added if guards in 'include.h', added 'alloc.c'

---
+++ src/alloc.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+void *checked(void *p) {
+    if(p == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    return p;
+}
+
+void *xmalloc(long long int n) {
+    return checked(malloc(n));
+}
---
+++ src/include.h
@@ -0,0 +1,6 @@
+#ifdef INCLUDE_H
+#define INCLUDE_H
+
+void *xmalloc(long long int n);
+
+#endif //INCLUDE_H
