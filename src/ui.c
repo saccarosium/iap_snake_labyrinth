233fe066f3e0dbde308fa9f478ddc25e0bd39466
saccarosium <github.e41mv@aleeas.com>
2022-10-24 22:49:12 +0200

    Refactored 'ui_create_win'

--- src/ui.c
+++ src/ui.c
@@ -16,7 +16,6 @@ typedef struct win {
     WINDOW *winid;
 } win;
 
-void ui_create_menu(win frame) {
 
 win *ui_get_term_info() {
     win *term = xmalloc(sizeof(win));
@@ -29,39 +28,42 @@ win *ui_get_term_info() {
     return term;
 }
 
-void ui_create_win(char *maze) {
+void ui_size_error() {
+    char *error = "Screen too small!";
+    win *term = ui_get_term_info();
+    mvprintw(term->center.y, (term->max_x - strlen(error)) / 2, error);
+}
+
+win *ui_create_win(int h, int w) {
     int min_col, min_row;
-    win term;
-    win frame;
+    win *term = ui_get_term_info();
+    win *frame = xmalloc(sizeof(win));
 
     // Minimum
     min_row = 79;
     min_col = 24;
 
-    frame.max_y = 40;
-    frame.max_x = 100;
+    frame->max_y = h;
+    frame->max_x = w;
 
-    // Gets the current terminal max x and y
-    getbegyx(stdscr, term.min_y, term.min_x);
-    getmaxyx(stdscr, term.max_y, term.max_x);
+    if (term->max_y >= min_col || term->max_x >= min_row) {
+        frame->min_y = (term->max_y - frame->max_y) / 2;
+        frame->min_x = (term->max_x - frame->max_x) / 2;
 
-    if (term.max_y < min_col || term.max_x < min_row) {
-        frame = term;
-        maze = "Too Smoll!";
+        frame->center.y = frame->max_y / 2;
+        frame->center.x = frame->max_x / 2;
+
+        // initialize the WINDOW
+        frame->winid = newwin(frame->max_y, frame->max_x, frame->min_y, frame->min_x);
+        refresh();
+        box(frame->winid, 0, 0);
+        wrefresh(frame->winid);
     } else {
-        frame.min_y = (term.max_y - frame.max_y) / 2;
-        frame.min_x = (term.max_x - frame.max_x) / 2;
+        ui_size_error();
     }
+    return frame;
+}
 
-    frame.center.y = frame.max_y / 2;
-    frame.center.x = frame.max_x / 2;
-
-
-    // initialize the WINDOW
-    WINDOW *win = newwin(frame.max_y, frame.max_x, frame.min_y, frame.min_x);
-    refresh();
-    box(win, 0, 0);
-
-    mvwprintw(win, frame.center.y, (frame.max_x - strlen(maze)) / 2, maze);
-    wrefresh(win);
+void ui_init() {
+    ui_create_win(40, 100);
 }
