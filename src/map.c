e2bfedc0e30d88ec7019c67a06649916d503ecdf
LucaBarban <45258881+LucaBarban@users.noreply.github.com>
2022-10-25 18:58:02 +0200

    Created 3 Function's firms
    3 new map related function's firms created (yet to be implemented/still not finished). .gitignore modified to make shure .vscode folders won't be uploaded. map.h updated to have a better names

--- src/map.c
+++ src/map.c
@@ -17,3 +17,51 @@ map *map_create(int height, int width) {
 node *map_get_node(map* m, int y, int x) {
     return &m->grid[y * m->width + x];
 }
+
+map* map_load_from_file(char* filename){
+    int c;
+    FILE *file;
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        return NULL;
+    }
+    
+    int buffer_size = 32;
+    int currently_used = 0;
+    char* read_buffer = malloc(sizeof(char) * buffer_size);
+    if (read_buffer == NULL){
+        return NULL;
+    }
+    char* write_head = read_buffer;
+    
+
+    //printf("facendo");
+    while ((c = getc(file)) != EOF){
+        //putchar(c);
+        printf("char: %c ==> %d-%d  (%d)\n", c, currently_used ,write_head, buffer_size);
+        *write_head = c;
+        write_head++;
+        currently_used++;
+        if (currently_used == buffer_size){
+            buffer_size += sizeof(char) * 32; //aggiungi altri 32 slot
+            read_buffer= realloc(read_buffer, buffer_size );
+            if (read_buffer == NULL) {
+                return NULL;
+            }
+        }
+    }
+    printf("currently_used: %d, buffer_size: %d", currently_used, buffer_size);
+    fclose(file);
+    printf("done");
+    for (int i = 0; i < currently_used; i++){
+        putchar(*(read_buffer + i));
+    }
+}
+
+void map_get_nearby_nodes(int x, int y, node *return_nodes){
+
+}
+
+void map_set_node_type(node* n, enum nodeType t){
+    n->type = t;
+}
--- src/map.h
+++ src/map.h
@@ -1,14 +1,17 @@
 #ifndef MAP_H
 #define MAP_H
 
-enum type {
+#include <stdlib.h>
+#include <stdio.h>
+
+enum nodeType{
     EMPTY,
     WALL,
     COIN,
 };
 
 typedef struct node {
-    enum type type;
+    enum nodeType type;
 } node;
 
 typedef struct map {
@@ -19,5 +22,8 @@ typedef struct map {
 
 map *map_create(int height, int width);
 node *map_get_node(map* m, int y, int x);
+map* map_load_from_file(char* filename);
+void map_get_nearby_nodes(int x, int y, node *return_nodes);
+void map_set_node_type(node* n, enum nodeType t);
 
 #endif // !MAP_H
