293c10882a9fde57283d352c611e1a9a3ba89bb3
LucaBarban <45258881+LucaBarban@users.noreply.github.com>
2022-11-02 11:04:29 +0100

    Adding error checking in map_load_from_filr
    Adding an int pointer to map_load_from_file to comunicate specific errors when they do occut

--- src/map.c
+++ src/map.c
@@ -40,79 +40,102 @@ nodeType get_character_type(char c) {
     }
 }
 
-map *map_load_from_file(char *filename) {
-    int map_width = 0, map_height = 0, width_check = 0, tmp_type;
+map *map_load_from_file(char *filename, int *error_code) {
+    *error_code = 0;
+    int map_width = 0, map_height = 0, width_check = 0, tmp_type, c;
 
-    int c;
+    
+    char *read_buffer;
+    int buffer_size = 32;
+    int currently_used = 0;
+    
     FILE *file;
     file = fopen(filename, "r");
     if (file == NULL) {
-        return NULL;
-    }
+        *error_code = -1;
+    } else {
+        read_buffer = malloc(sizeof(char) * buffer_size);
+        if (read_buffer == NULL) {
+            *error_code = -2;
+        } else {
+            while ((c = getc(file)) != EOF) {
+                tmp_type = get_character_type(c);
+                if (tmp_type == -2) {
+                    *error_code = -3;
+                    break;
+                }
 
-    int buffer_size = 32;
-    int currently_used = 0;
-    char *read_buffer = malloc(sizeof(char) * buffer_size);
-    if (read_buffer == NULL) {
-        return NULL;
-    }
-    while ((c = getc(file)) != EOF) {
-        tmp_type = get_character_type(c);
-        if (tmp_type == -2) {
-            return NULL;
-        }
+                if (tmp_type != -1) {
+                    if (!map_height)
+                        width_check = ++map_width;
+                    else if (map_width + 1 <= width_check)
+                        map_width++;
+                    else {
+                        *error_code = -4;
+                        break;
+                    }
+                } else if (map_width != 0) {
+                    if (map_width != width_check) {
+                        *error_code = -4;
+                        break;
+                    }
+                    map_height++;
+                    map_width = 0;
+                }
 
-        if (tmp_type != -1) {
-            if (!map_height)
-                width_check = ++map_width;
-            else if (map_width + 1 <= width_check)
-                map_width++;
-            else {
-                return NULL;
+                if (tmp_type >= 0) {
+                    read_buffer[currently_used] = tmp_type;
+                    currently_used += sizeof(char);
+                }
+
+                if (currently_used == buffer_size) {
+                    buffer_size += sizeof(char) * 32;
+                    read_buffer = realloc(read_buffer, buffer_size);
+                    if (read_buffer == NULL) {
+                        *error_code = -5;
+                        break;
+                    }
+                }
             }
-        } else if (map_width != 0) {
-            if (map_width != width_check) {
-                return NULL;
-            }
-            map_height++;
-            map_width = 0;
-        }
 
-        if (tmp_type >= 0) {
-            read_buffer[currently_used] = tmp_type;
-            currently_used += sizeof(char);
-        }
+            fclose(file);
 
-        if (currently_used == buffer_size) {
-            buffer_size += sizeof(char) * 32;
-            read_buffer = realloc(read_buffer, buffer_size);
-            if (read_buffer == NULL) {
-                return NULL;
+            if (*error_code == 0) {
+                if (map_width != width_check && map_width != 0) {
+                    *error_code = -6;
+                } else {
+                    map_width = width_check;
+                    map_height++;
+
+                    map *m = map_create(map_height, width_check);
+
+                    for (int i = 0; i < map_height; i++) {
+                        for (int j = 0; j < map_width; j++) {
+                            m->grid[i * map_width + j].type =
+                                read_buffer[i * map_width + j];
+                        }
+                    }
+                    return m;
+                }
             }
         }
     }
-
-    fclose(file);
-
-    if (map_width != width_check && map_width != 0) {
+    switch (*error_code) {
+    case -1:
+        break;
+    case -2:
+    case -5:
+        fclose(file);
+        break;
+    case -3:
+    case -4:
+    case -6:
+        fclose(file);
         free(read_buffer);
-        return NULL;
+        break;
+    default:
+        break;
     }
-    map_width = width_check;
-    map_height++;
-
-    map *m = map_create(map_height, width_check);
-
-    for (int i = 0; i < map_height; i++) {
-        for (int j = 0; j < map_width; j++) {
-            tmp_type = read_buffer[i * map_width + j];
-            if (tmp_type < 0) {
-                return NULL;
-            }
-            m->grid[i * map_width + j].type = tmp_type;
-        }
-    }
-    return m;
 }
 
 void map_get_nearby_nodes(int x, int y, node *return_nodes) {
