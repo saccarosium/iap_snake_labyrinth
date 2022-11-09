a950152ca8c61fb24c8fa971d2bacd6c1ae4333a
Friedchicken-42 <paololego20+github@gmail.com>
2022-11-09 10:51:48 +0100

    added initial main template

--- src/main.c
+++ src/main.c
@@ -1,7 +1,35 @@
 #include <ncurses.h>
 
 int main() {
-    initscr();
+    /* Template:
+    ui *u = ui_init();
 
+    // ui menu (choose gamemode)
+
+    error *err = OK;
+    game *g = game_init(&err);
+
+    // error loading game
+    if(err != OK) {
+        ui_error(u, err);
+    }
+
+    // game loop
+    while(!game_ended(g)) {
+        char keypress = ui_get_input();
+        game_update(g, keypress, &err);
+        // game update puo' ritornare un errore?
+
+        ui_update(g, err);
+        if(err != OK) break;
+    }
+
+    if(err != OK) {
+        // stuff
+    }
+
+    game_cleanup(g);
+    ui_cleanup(u);
+    */
     return 0;
 }
