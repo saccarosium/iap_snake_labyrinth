e353e9f974a4f2583676e91682611d26a65bc60b
saccarosium <github.e41mv@aleeas.com>
2022-12-14 23:57:32 +0100

    Added testing for all mazes in script

--- scripts/challenge.sh
+++ scripts/challenge.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
-file="./assets/maze1.txt"
-maze="$(cat $file)"
-rows="$(echo "$maze" | wc -l)"
-collumns="$(tail -1 $file | wc -m)"
-collumns="$((collumns - 1))"
+for i in $(seq 1 2); do
+    file="./assets/maze$i.txt"
+    maze="$(cat "$file")"
+    rows="$(echo "$maze" | wc -l)"
+    collumns="$(tail -1 "$file" | wc -m)"
+    collumns="$((collumns - 1))"
 
-echo "$collumns"
-echo "$rows"
-echo "$maze"
+    echo "$collumns"
+    echo "$rows"
+    echo "$maze"
 
-./build/maze --challenge << EOF 
+./build/maze --challenge <<EOF
 $collumns
 $rows
 $maze
 EOF
+
+done
